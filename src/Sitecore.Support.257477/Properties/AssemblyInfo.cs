7c4fb6ad32668e0ad780d027dced98bd3a3c54ab
DK\ELB <elb@sitecore.net>
2018-10-05 13:34:32 +0300

    test

---
+++ src/Sitecore.Support.257477/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.257477")]
+[assembly: AssemblyProduct("Sitecore.Support.257477")]
+[assembly: ComVisible(false)]
